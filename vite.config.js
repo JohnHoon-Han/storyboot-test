5fa2ad807a7d9bc5688c056e64952f5c278a10b1
jhhan <jhhan@in-soft.co.kr>
2025-04-14 17:22:23 +0900

    빌드 설정 파일 추가

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    // plugins: [dts()]
+    plugins: [
+        react(),
+        tailwindcss(),
+        dts(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/index.css',
+                    dest: '',
+                },
+            ],
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.tsx'),
+            name: 'hoons-design-system',
+            fileName: function (format) { return "index.".concat(format, ".js"); },
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDom',
+                },
+            },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+        outDir: 'dist',
+    },
+});
